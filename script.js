c8c948f38e842763183a5ca82acd72f91f7583da
Olek <aleksander.galdyn@o2.pl>
2023-05-06 23:15:47 +0200

    add switching players

--- script.js
+++ script.js
@@ -1,41 +1,81 @@
 'use strict';
 
+// --MAIN VARIABLES--
+
+// buttons
 const btnNewGame = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+//dice image
 const diceImg = document.querySelector('.dice');
-const totalScorePlayer1 = document.getElementById('score--0');
+//score elements
+const totalScorePlayer0 = document.getElementById('score--0');
+const totalScorePlayer1 = document.getElementById('score--1');
+const currentScorePlayer0 = document.getElementById('current--0');
+const currentScorePlayer1 = document.getElementById('current--1');
+//players background
+const backgroundPlayer0 = document.querySelector('.player--0');
+const backgroundPlayer1 = document.querySelector('.player--1');
 
-let currentScorePlayer1 = document.getElementById('current--0');
+// --/MAIN VARIABLES--
 
-scorePlayer1.textContent = '0';
+//reset
+totalScorePlayer0.textContent = 0;
+totalScorePlayer1.textContent = 0;
+diceImg.style = 'display: none';
 
+//scores
+let activePlayer = 0;
+let currentScore = 0;
+const scores = [0, 0];
+
+//background switch
+const backgroundSwitch = () => {
+  backgroundPlayer0.classList.toggle('player--active');
+  backgroundPlayer1.classList.toggle('player--active');
+};
+
+//dice roll function
 const rollDice = () => {
   const rollDiceNumber = Math.trunc(Math.random() * 6) + 1;
   diceImg.src = `dice-${rollDiceNumber}.png`;
-  if (rollDiceNumber === 1) {
-    currentScorePlayer1.textContent = '0';
+  diceImg.style = 'display: block';
+
+  if (rollDiceNumber !== 1) {
+    currentScore += rollDiceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    currentScorePlayer1.textContent =
-      Number(currentScorePlayer1.textContent) + rollDiceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    backgroundSwitch();
   }
-  console.log(currentScorePlayer1.textContent);
 };
 
 btnRoll.addEventListener('click', rollDice);
 
+//hold score function
 const holdScore = () => {
-  totalScorePlayer1.textContent =
-    Number(totalScorePlayer1.textContent) +
-    Number(currentScorePlayer1.textContent);
-  currentScorePlayer1.textContent = '0';
+  document.getElementById(`score--${activePlayer}`).textContent =
+    Number(document.getElementById(`score--${activePlayer}`).textContent) +
+    currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  backgroundSwitch();
 };
 
 btnHold.addEventListener('click', holdScore);
 
+//new game function
 const newGame = () => {
-  currentScorePlayer1.textContent = '0';
-  totalScorePlayer1.textContent = '0';
+  totalScorePlayer0.textContent = 0;
+  totalScorePlayer1.textContent = 0;
+  currentScorePlayer0.textContent = 0;
+  currentScorePlayer1.textContent = 0;
+  diceImg.style = 'display: none';
+  activePlayer = 0;
 };
 
 btnNewGame.addEventListener('click', newGame);
