d1b7938052ee9162ab7f54a1b25bf988f3af2166
Olek <aleksander.galdyn@o2.pl>
2023-05-06 20:35:45 +0200

    done logic for player1

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 'use strict';
 
+const btnNewGame = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const diceImg = document.querySelector('.dice');
-const scorePlayer1 = document.getElementById('score--0');
+const totalScorePlayer1 = document.getElementById('score--0');
 
 let currentScorePlayer1 = document.getElementById('current--0');
 
 scorePlayer1.textContent = '0';
 
-const rollDice = function () {
+const rollDice = () => {
   const rollDiceNumber = Math.trunc(Math.random() * 6) + 1;
   diceImg.src = `dice-${rollDiceNumber}.png`;
   if (rollDiceNumber === 1) {
@@ -23,10 +24,18 @@ const rollDice = function () {
 
 btnRoll.addEventListener('click', rollDice);
 
-const holdScore = function () {
-  scorePlayer1.textContent =
-    Number(scorePlayer1.textContent) + Number(currentScorePlayer1.textContent);
+const holdScore = () => {
+  totalScorePlayer1.textContent =
+    Number(totalScorePlayer1.textContent) +
+    Number(currentScorePlayer1.textContent);
   currentScorePlayer1.textContent = '0';
 };
 
 btnHold.addEventListener('click', holdScore);
+
+const newGame = () => {
+  currentScorePlayer1.textContent = '0';
+  totalScorePlayer1.textContent = '0';
+};
+
+btnNewGame.addEventListener('click', newGame);
